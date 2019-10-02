b6151a5940095ba6d8a64b001b15eaca8c20f603
Richard Jones <richard@cottagelabs.com>
2019-10-02 21:55:56 +0100

    add inline markdown renderer

--- compost/renderers/md.py
+++ compost/renderers/md.py
@@ -10,3 +10,15 @@ class MarkdownRenderer(Renderer):
     def render(self, text):
         body = markdown.markdown(text, extensions=self._settings.get("extensions", []))
         return body
+
+class MarkdownInlineRenderer(MarkdownRenderer):
+    def __init__(self, cfg_id):
+        super(MarkdownInlineRenderer, self).__init__(cfg_id)
+
+    def render(self, text):
+        body = super(MarkdownInlineRenderer, self).render(text)
+        if body.startswith("<p>"):
+            body = body[3:]
+        if body.endswith("</p>"):
+            body = body[:-4]
+        return body
